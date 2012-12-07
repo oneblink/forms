d72f1907668cf9a4ea5b407f0f80d467fcf8dbf5
Ron Waldon <ron@blinkmobile.com.au>
2012-12-07 17:51:59 +1100

    early progress at getting dynamic dependency loading moved requirejs file to help compatibility with bower

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
-requirejs.config({
+var cdn = 'https://d1c6dfkb81l78v.cloudfront.net/';
+
+require.config({
+  baseUrl: 'js',
+  paths: {
+    backbone: cdn + 'backbonejs/0.9.2/backbone.min',
+    jquery: cdn + 'jquery/1.8.3/jq.min',
+    underscore: cdn + 'underscorejs/1.4.3/u.min'
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    underscore: {
+      exports: '_'
+    }
+  },
+  enforceDefine: true
 });
 
-define([], function() {
+define(['jquery'], function($) {
+  Forms = window.BlinkForms = {};
 });
