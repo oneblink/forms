d000ec3d92e2c79e45d23d528f227c7b09876bba
Ron Waldon <ron@blinkmobile.com.au>
2012-12-13 14:37:03 +1100

    temporary framework for adding any number of tests to the runner

--- test/runner.js
+++ test/runner.js
@@ -1,18 +1,24 @@
 
 define(['mocha', 'q'], function(mocha, Q) {
   'use strict';
-  var tests = Q.defer();
+  var tests = [],
+      promises;
 
   mocha.setup({
     ui: 'tdd',
     globals: ['Backbone', 'BlinkForms']
   });
 
+  tests.push(Q.defer());
   require(['../test/1/test'], function() {
-    tests.resolve();
+    tests[0].resolve();
   });
 
-  tests.promise.then(function() {
+  promises = _.map(tests, function(dfrd) {
+    return dfrd.promise;
+  });
+
+  Q.all(promises).then(function() {
     mocha.run();
   });
 });
