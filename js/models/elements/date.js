a081da3bbc1a15182c0c5db98b0a91b9ebf73212
Ron Waldon <ron@blinkmobile.com.au>
2013-01-29 12:02:30 +1100

    override DateElement.set() and get() to comply with old API whilst shielding DOM from weird data - TODO: fix this for old non-ISO8601 browsers - TODO: fix DateElementView for non-HTML5 browsers

--- js/models/elements/date.js
+++ js/models/elements/date.js
@@ -5,6 +5,51 @@ define(['underscore', 'backbone', 'models/element'],
   var DateElement = Element.extend({
     initialize: function() {
       Element.prototype.initialize.call(this);
+    },
+    /**
+     * override get to handle date formats
+     */
+    get: function(key) {
+      var val = Backbone.Model.prototype.get.call(this, key),
+          date;
+
+      if (key !== 'value') {
+        return val;
+      }
+      // create complete ISO8601 string as per our legacy API
+      // TODO: fix this for legacy browsers lacking ISO8601 support
+      date = new Date(val);
+      if (date.valueOf() === date.valueOf()) {
+        val = date.toISOString().replace(/\.\d\d\d/, '');
+      }
+      return val;
+    },
+    /**
+     * override set to handle date formats
+     */
+    set: function(key, val, opts) {
+      var attrs,
+          date;
+
+      if (key == null) return this;
+      if (typeof key === 'object') {
+        attrs = key;
+        opts = val;
+      } else {
+        (attrs = {})[key] = val;
+      }
+      opts || (opts = {});
+      if ('value' in attrs && attrs.value) {
+        // check for valid date value
+        // TODO: fix this for legacy browsers lacking ISO8601 support
+        date = new Date(attrs.value);
+        if (date.valueOf() !== date.valueOf()) {
+          return this;
+        }
+        // crop time and zone from date
+        attrs.value = date.toISOString().replace(/T.*$/, '');
+      }
+      return Backbone.Model.prototype.set.call(this, attrs, opts);
     }
   });
 
