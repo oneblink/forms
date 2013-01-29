13c6d80ffeddfdabf73393de6ba52e3525262811
Ron Waldon <ron@blinkmobile.com.au>
2013-01-29 16:51:02 +1100

    use Grunt to do things that can't be done in Require.JS

--- grunt.js
+++ grunt.js
@@ -5,6 +5,8 @@
 module.exports = function(grunt) {
   'use strict';
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-jslint');
 
@@ -18,10 +20,14 @@ module.exports = function(grunt) {
         'js/config.js',
         'node_modules/**',
         'js/lib/**',
-        '**/*.min.js'
+        '**/*.min.js',
+        'js/build/**'
       ],
       directives: {
         browser: true,
+        es5: true,
+        nomen: true,
+        todo: true,
         white: true,
         predef: [
           // pre-defined globals
@@ -39,14 +45,17 @@ module.exports = function(grunt) {
       compile: {
         options: {
           baseUrl: 'js',
+          dir: 'js/build',
           mainConfigFile: 'js/config.js',
           optimize: 'uglify',
+//          optimize: 'none', // debug-only
           uglify: {
             max_line_length: 80
           },
           paths: {
             backbone: 'empty:',
             jquery: 'empty:',
+            jquerymobile: 'empty:',
             underscore: 'empty:',
             // testing libraries
             chai: 'empty:',
@@ -54,17 +63,43 @@ module.exports = function(grunt) {
             // boot / entry points
             main: 'main'
           },
-          name: 'main',
-          include: ['config', 'boot'],
-          out: 'js/main.min.js'
+          modules: [
+            {
+              name: 'main',
+              include: ['config', 'boot'],
+              out: 'js/build/main.min.js'
+            },
+            {
+              name: 'views/jqm',
+              include: ['main', 'config', 'boot'],
+              out: 'js/build/jqm.min.js'
+            }
+          ]
         }
+      }
+    },
+
+    clean: ['js/build'],
+
+    copy: {
+      main: {
+        files: [
+          {
+            src: 'js/build/main.js',
+            dest: 'js/core.min.js'
+          },
+          {
+            src: 'js/build/views/jqm.js',
+            dest: 'js/jqm.min.js'
+          }
+        ]
       }
     }
 
   });
 
   grunt.registerTask('default', 'jslint');
-  grunt.registerTask('build', 'jslint requirejs');
+  grunt.registerTask('build', 'jslint requirejs copy clean');
 
 };
 
