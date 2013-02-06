658ae9a6f48560cc3ee193120b18bf5addc7f073
Ron Waldon <ron@blinkmobile.com.au>
2013-02-06 17:51:57 +1100

    NumberElement now displays with a slider if "min" and "max" settings are present

--- js/models/elements/number.js
+++ js/models/elements/number.js
@@ -4,6 +4,26 @@ define(['models/element'], function(Element) {
   var NumberElement = Element.extend({
     initialize: function() {
       Element.prototype.initialize.call(this);
+    },
+    set: function(key, val, options) {
+      var attr, attrs, unset, changes, silent, changing, prev, current;
+      if (key === null) {
+        return this;
+      }
+      if (_.isObject(key)) {
+        attrs = key;
+        options = val;
+      } else {
+        (attrs = {})[key] = val;
+      }
+      options = options || {};
+
+      // tamper with 'value' if present
+      if (attrs.value !== 'undefined' && attrs.value !== null) {
+        attrs.value = Number(attrs.value);
+      }
+
+      return Element.prototype.set.call(this, attrs, options);
     }
   });
 
--- js/views/jqm/elements/number.js
+++ js/views/jqm/elements/number.js
@@ -4,19 +4,44 @@ define(['views/jqm/element'], function(ElementView) {
   var NumberElementView = ElementView.extend({
     render: function() {
       var $input,
-          name = this.model.get('name');
+          attrs = this.model.attributes,
+          name = attrs.name,
+          min = attrs.min,
+          max = attrs.max,
+          step = attrs.step || 1;
 
       this.$el.empty();
       this.renderLabel();
 
-      $input = $('<input type="number" />');
+      if (_.isNumber(min) && _.isNumber(max)) {
+        $input = $('<input type="range" />');
+        $input.attr({
+          min: min,
+          max: max,
+          'data-highlight': true
+        });
+//        this.model.on('change:value', this.onValueChange, this);
+
+        $(document).one('pageinit', $.proxy(this.bindRivets, this));
+
+      } else {
+        // TODO: HTML4-fallback for buggy HTML5 browsers
+        $input = $('<input type="number" />');
+      }
       $input.attr({
         name: name,
-        'data-rv-value': 'm.value'
+        'data-rv-value': 'm.value',
+        step: step
       });
       this.$el.append($input);
       this.bindRivets();
-    }
+   },
+   onValueChange: function() {
+      var view = this,
+          $inputs = view.$el.find('input');
+
+      $inputs.slider('refresh');
+   }
   });
 
   return NumberElementView;
