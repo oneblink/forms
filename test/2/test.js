775bd00252b6361df02f603239472a5e73ffc116
Ron Waldon <ron@blinkmobile.com.au>
2013-02-12 09:44:35 +1100

    add unit tests for the Choice elements

--- test/2/test.js
+++ test/2/test.js
@@ -47,7 +47,85 @@ define(['q', 'BlinkForms', 'BIC'], function(Q, Forms) {
         $.mobile.page({}, $page);
         $page.trigger('pagecreate');
         $page.show();
-        });
+      });
+
+      test('select-1 collapsed', function() {
+        var form = Forms.currentFormObject,
+            element = form.getElement('selectc'),
+            $fieldset = element.attributes._view.$el,
+            $span = $fieldset.find('.ui-btn-text');
+
+        element.val('');
+        assert.equal(element.val(), '', 'model.value is ""');
+
+        element.val('a');
+        assert.equal(element.val(), 'a', 'model.value = "a"');
+        assert.equal($span.text(), 'alpha', 'jQM shows "alpha"');
+
+        element.val('b');
+        assert.equal(element.val(), 'b', 'model.value = "b"');
+        assert.equal($span.text(), 'beta', 'jQM shows "beta"');
+
+        // TODO: test UI causes side-effect in model
+      });
+
+      test('select-multi collapsed', function() {
+        var form = Forms.currentFormObject,
+            element = form.getElement('multic'),
+            $fieldset = element.attributes._view.$el,
+            $span = $fieldset.find('.ui-btn-text');
+
+        element.val([]);
+        assert.deepEqual(element.val(), [], 'model.value is []');
+
+        element.val(['a']);
+        assert.deepEqual(element.val(), ['a'], 'model.value = ["a"]');
+        assert.equal($span.text(), 'alpha', 'jQM shows "alpha"');
+
+        element.val(['a', 'b']);
+        assert.deepEqual(element.val(), ['a', 'b'], 'model.value = ["a", "b"]');
+        assert.equal($span.text(), 'alpha, beta', 'jQM shows "alpha, beta"');
+
+        // TODO: test UI causes side-effect in model
+      });
+
+      test('select-1 expanded', function() {
+        var form = Forms.currentFormObject,
+            element = form.getElement('selecte'),
+            $fieldset = element.attributes._view.$el;
+
+        element.val('');
+        assert.equal(element.val(), '', 'model.value is ""');
+        assert.equal($fieldset.find('ui-btn-active').length, 0, 'jQM is blank');
+
+        element.val('a');
+        assert.equal(element.val(), 'a', 'model.value = "a"');
+
+        element.val('b');
+        assert.equal(element.val(), 'b', 'model.value = "b"');
+
+        $fieldset.find('label:contains(gamma)').trigger('click');
+        assert.equal(element.val(), 'g', 'model.value = "g"');
+      });
+
+      test('select-multi expanded', function() {
+        var form = Forms.currentFormObject,
+            element = form.getElement('multie'),
+            $fieldset = element.attributes._view.$el;
+
+        element.val('');
+        assert.equal(element.val(), '', 'model.value is ""');
+        assert.equal($fieldset.find('ui-btn-active').length, 0, 'jQM is blank');
+
+        element.val(['a']);
+        assert.deepEqual(element.val(), ['a'], 'model.value = ["a"]');
+
+        element.val(['a', 'b']);
+        assert.deepEqual(element.val(), ['a', 'b'], 'model.value = ["a", "b"]');
+
+        $fieldset.find('.ui-btn-text:contains(beta)').trigger('click');
+        $fieldset.find('.ui-btn-text:contains(gamma)').trigger('click');
+        assert.deepEqual(element.val(), ['a', 'g'], 'model.value = ["a", "g"]');
       });
 
     }); // END: suite('Form', ...)
