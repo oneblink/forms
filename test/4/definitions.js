6992d92b592dc1a9063185d239536638fd6a32c5
Ron Waldon <ron@blinkmobile.com.au>
2013-02-27 10:55:13 +1100

    add nested Sections to test scenario #4

--- test/4/definitions.js
+++ test/4/definitions.js
@@ -28,10 +28,48 @@ define(function() {
           },
           {
             default: {
+              name: 'age',
+              label: 'Age',
+              type: 'number',
+              section: 'col1'
+            }
+          },
+          {
+            default: {
+              name: 'weight',
+              label: 'Weight',
+              type: 'number',
+              section: 'col2'
+            }
+          },
+          {
+            default: {
               name: 'comments',
               label: 'Comments',
               type: 'subForm',
               subForm: 'form2'
+            }
+          }
+        ],
+        _sections: [
+          {
+            default: {
+              name: 'grid',
+              class: 'ui-responsive ui-grid-a'
+            }
+          },
+          {
+            default: {
+              name: 'col1',
+              class: 'ui-block-a',
+              section: 'grid'
+            }
+          },
+          {
+            default: {
+              name: 'col2',
+              class: 'ui-block-b',
+              section: 'grid'
             }
           }
         ]
