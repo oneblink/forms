934bed0ce5f62988a353e6447c254e391a0fd1d9
Ron Waldon <ron@blinkmobile.com.au>
2013-05-29 11:01:07 +1000

    implement all Expression operators used by Forms v2

--- js/models/expression.js
+++ js/models/expression.js
@@ -50,7 +50,6 @@ define(function (require) {
       return !!arg;
     });
   };
-
   Expression.fn.or = function () {
     var args = Array.prototype.slice.call(arguments);
 
@@ -58,20 +57,70 @@ define(function (require) {
       return !!arg;
     });
   };
-
-  Expression.fn.empty = function () {
-    var args = Array.prototype.slice.call(arguments);
-
-    return args.every(function (arg) {
-      return _.isEmpty(arg);
-    });
+  Expression.fn.not = function (a) {
+    return !a;
   };
 
-  Expression.fn['!empty'] = function () {
-    var args = Array.prototype.slice.call(arguments);
-
-    return !Expression.fn.empty.apply(this, args);
+  Expression.fn.empty = function (a) {
+    if (_.isObject(a) || _.isString(a) || _.isArray(a)) {
+      return _.isEmpty(a);
+    }
+    if (_.isNumber(a)) {
+      return _.isNaN(a);
+    }
+    return !a;
   };
+  Expression.fn['!empty'] = function (a) {
+    return !Expression.fn.empty.call(this, a);
+  };
+
+  /*jslint eqeq:true*/
+  Expression.fn['=='] = function (a, b) {
+    return a == b;
+  };
+  Expression.fn['!='] = function (a, b) {
+    return a != b;
+  };
+  /*jslint eqeq:false*/
+
+  Expression.fn['<'] = function (a, b) {
+    return a < b;
+  };
+  Expression.fn['<='] = function (a, b) {
+    return a <= b;
+  };
+  Expression.fn['>'] = function (a, b) {
+    return a > b;
+  };
+  Expression.fn['>='] = function (a, b) {
+    return a >= b;
+  };
+
+  Expression.fn.contains = function (haystack, needle) {
+    var found;
+    if (!haystack || _.isEmpty(haystack)) {
+      return false;
+    }
+    if (typeof haystack === 'string') {
+      return haystack.indexOf(needle) !== -1;
+    }
+    /*jslint eqeq:true*/
+    if (_.isArray(haystack)) {
+      found = false;
+      haystack.forEach(function (item) {
+        if (item == needle) {
+          found = true;
+        }
+      });
+      return found;
+    }
+    /*jslint eqeq:false*/
+    throw new Error('contains: unexpected operand type');
+  };
+  Expression.fn['!contains'] = function (haystack, needle) {
+    return !Expression.fn.contains.call(this, haystack, needle);
+  };
+
 
   return Expression;
 });
