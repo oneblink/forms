b399c4ebb9212f7e872d0a4f64c87e7b6ab7581c
Ron Waldon <ron@blinkmobile.com.au>
2013-06-05 12:32:37 +1000

    ElementViews are initialised by Model constructor, not in static factory

--- js/models/element.js
+++ js/models/element.js
@@ -16,6 +16,8 @@ define(function (require) {
         page = attrs.page,
         section = $.trim(attrs.section || '');
 
+      this.initializeView();
+
       if (form) {
         page = attrs.page = form.getPage(attrs.page);
         if (page && section) {
@@ -52,17 +54,91 @@ define(function (require) {
       }
       return _.isEmpty(errors) ? undefined : errors;
     },
-    destroy: function (options) {
+    removeView: function () {
       var attrs = this.attributes;
       if (attrs._view) {
         attrs._view.remove();
         delete attrs._view;
       }
+    },
+    destroy: function (options) {
+      var attrs = this.attributes;
       delete attrs.form;
       delete attrs.page;
       delete attrs.section;
       this.id = null; // to prevent "sync"
       return Backbone.Model.prototype.destroy.call(this, options);
+    },
+    /**
+     * this will close any existing view first, then establish a new view
+     */
+    initializeView: function () {
+      var Forms = BMP.Forms,
+        dateTypes = ['date', 'time', 'datetime'],
+        attrs = this.attributes,
+        View,
+        view,
+        mode;
+
+      this.removeView();
+
+      switch (attrs.type) {
+      case 'subForm':
+        View = Forms._views.SubFormElement;
+        break;
+      case 'heading':
+        View = Forms._views.HeadingElement;
+        break;
+      case 'message':
+        View = Forms._views.MessageElement;
+        break;
+      case 'boolean':
+        View = Forms._views.BooleanElement;
+        break;
+      case 'select':
+        mode = attrs.mode;
+        mode = mode[0].toUpperCase() + mode.substring(1);
+        View = Forms._views['Choice' + mode + 'Element'];
+        break;
+      case 'multi':
+        mode = attrs.mode;
+        mode = mode[0].toUpperCase() + mode.substring(1);
+        View = Forms._views['Choice' + mode + 'Element'];
+        break;
+      case 'hidden':
+        View = Forms._views.HiddenElement;
+        break;
+      case 'number':
+        View = Forms._views.NumberElement;
+        break;
+      case 'telephone':
+        View = Forms._views.TelephoneElement;
+        break;
+      case 'password':
+        View = Forms._views.PasswordElement;
+        break;
+      case 'email':
+        View = Forms._views.EmailElement;
+        break;
+      case 'url':
+        View = Forms._views.URLElement;
+        break;
+      case 'text':
+        View = Forms._views.TextElement;
+        break;
+      case 'textarea':
+        View = Forms._views.TextAreaElement;
+        break;
+      default:
+        if (_.indexOf(dateTypes, attrs.type) !== -1) {
+          View = Forms._views.DateElement;
+        } else {
+          View = Forms._views.Element;
+        }
+      }
+      view = new View({model: this});
+      this.set('_view', view);
+      return view;
     },
     /**
      * official Blink API
@@ -87,7 +163,6 @@ define(function (require) {
         view,
         el,
         TypedElement,
-        View,
         mode;
 
       if (!attrs || !_.isObject(attrs)) {
@@ -100,77 +175,55 @@ define(function (require) {
       switch (attrs.type) {
       case 'subForm':
         TypedElement = Forms._models.SubFormElement;
-        View = Forms._views.SubFormElement;
         break;
       case 'heading':
         TypedElement = Forms._models.HeadingElement;
-        View = Forms._views.HeadingElement;
         break;
       case 'message':
         TypedElement = Forms._models.MessageElement;
-        View = Forms._views.MessageElement;
         break;
       case 'boolean':
         TypedElement = Forms._models.BooleanElement;
-        View = Forms._views.BooleanElement;
         break;
       case 'select':
         TypedElement = Forms._models.SelectElement;
-        mode = attrs.mode;
-        mode = mode[0].toUpperCase() + mode.substring(1);
-        View = Forms._views['Choice' + mode + 'Element'];
         break;
       case 'multi':
         TypedElement = Forms._models.MultiElement;
-        mode = attrs.mode;
-        mode = mode[0].toUpperCase() + mode.substring(1);
-        View = Forms._views['Choice' + mode + 'Element'];
         break;
       case 'hidden':
         TypedElement = Forms._models.HiddenElement;
-        View = Forms._views.HiddenElement;
         break;
       case 'number':
         TypedElement = Forms._models.NumberElement;
-        View = Forms._views.NumberElement;
         break;
       case 'telephone':
         TypedElement = Forms._models.TelephoneElement;
-        View = Forms._views.TelephoneElement;
         break;
       case 'password':
         TypedElement = Forms._models.PasswordElement;
-        View = Forms._views.PasswordElement;
         break;
       case 'email':
         TypedElement = Forms._models.EmailElement;
-        View = Forms._views.EmailElement;
         break;
       case 'url':
         TypedElement = Forms._models.URLElement;
-        View = Forms._views.URLElement;
         break;
       case 'text':
         TypedElement = Forms._models.TextElement;
-        View = Forms._views.TextElement;
         break;
       case 'textarea':
         TypedElement = Forms._models.TextAreaElement;
-        View = Forms._views.TextAreaElement;
         break;
       default:
         if (_.indexOf(dateTypes, attrs.type) !== -1) {
           TypedElement = Forms._models.DateElement;
-          View = Forms._views.DateElement;
         } else {
           TypedElement = Forms._models.Element;
-          View = Forms._views.Element;
         }
       }
       // TODO: set View = read-only view for m.readOnly
       el = new TypedElement(attrs);
-      view = new View({model: el});
-      el.attributes._view = view;
       return el;
     }
   });
