ea1db1d6783b73c4eec97a990401e33db5d5f584
Ashish Tilara <ashish@itcutives.com>
2014-07-21 13:07:48 +1000

    FORMS-60 # Form Headings & message fields don't appear
    - added check to convert v2 field attributes into v3
    - added tests to see the fields are rendered properly

--- js/models/elements/heading.js
+++ js/models/elements/heading.js
@@ -4,9 +4,23 @@ define(['models/element'], function (Element) {
   var HeadingElement = Element.extend({
     defaults: {
       page: 0,
-      persist: false
+      persist: false,
+      level: 1
     },
     initialize: function () {
+      var self = this,
+        schemaMap = {
+          'headingText': 'text',
+          'headingPosition': 'position',
+          'headingFontFace': 'fontFace'
+        };
+
+      Object.keys(schemaMap).forEach(function (key) {
+        if (schemaMap[key] && self.attributes[key]) {
+          self.attributes[schemaMap[key]] = self.attributes[key];
+        }
+      });
+
       Element.prototype.initialize.call(this);
     }
   });
--- test/12_rivets/test.js
+++ test/12_rivets/test.js
@@ -1,5 +1,5 @@
 /*jslint browser:true, indent:2, devel:true*/
-/*global suite, test, setup, teardown, suiteSetup, suiteTeardown*/ // mocha
+/*global console, suite, test, setup, teardown, suiteSetup, suiteTeardown*/ // mocha
 /*global assert*/ // chai
 /*global define, require*/ // AMD / Require.JS
 
@@ -187,6 +187,15 @@ define(['jquery', 'BlinkForms', 'BIC'], function ($, Forms) {
       });
 
     }); // END: suite('Form', ...)
+
+    suite('Message and Headings', function () {
+      test('Message field with old-definition structure', function () {
+        assert.equal($('[data-name="_message_1"]').html(), "Message using <em><strong>old definition<strong> structure</strong></strong></em>");
+      });
+      test('Heading field with old-definition structure', function () {
+        assert.equal($('[data-name="_heading_1"]').html(), "Heading using old definition");
+      });
+    });
 
   }); // END: suite('12', ...)
 
