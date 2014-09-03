a9ba01db6378c7054a4b48116b90261039fbcef0
Ron Waldon <jokeyrhyme@gmail.com>
2014-09-03 14:35:25 +1000

    add tests for `BMP.Forms.version`

---
+++ test/17_main/test.js
@@ -0,0 +1,26 @@
+/*global suite:true, test:true*/ // mocha
+/*global assert:true*/ // chai
+
+define(['BlinkForms', 'BIC'], function () {
+
+  suite('17: main', function () {
+
+    test('BMP is defined', function () {
+      assert.isDefined(window.BMP);
+    });
+
+    test('BMP.Forms is defined', function () {
+      assert.isDefined(window.BMP.Forms);
+    });
+
+    test('BMP.Forms.initialize is a function', function () {
+      assert.isFunction(window.BMP.Forms.initialize);
+    });
+
+    test('BMP.Forms.version is a string', function () {
+      assert.isString(window.BMP.Forms.version);
+    });
+
+  });
+
+});
