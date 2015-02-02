6068c1bfe6f91f1e6039f3dc9efb0cf2f1eec81f
Geeta <geeta@blinkmobile.com.au>
2015-02-03 10:24:46 +1100

    Forms-92 : Forms 3: Location button, can we have a "Searching" label?
    - disabled location button after click till location is loaded
    - changed button text to 'locating...' till location is loaded

--- js/models/elements/location.js
+++ js/models/elements/location.js
@@ -31,49 +31,55 @@ define(['models/element'], function (Element) {
       if (!geolocation || !geolocation.getCurrentPosition) {
         errors.value.push('unsupported in this browser');
         self.set('errors', errors);
-        return;
+        return Promise.reject(new Error(errors.value[0]));
       }
-      options = $.isPlainObject(options) ? options : {};
-      options = $.extend({}, defaultOptions, options);
-      geolocation.getCurrentPosition(
-        function (position) { // successCallback
-          var coords;
-          if (position.coords) {
-            coords = {
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-              altitude: position.coords.altitude,
-              accuracy: position.coords.accuracy,
-              altitudeAccuracy: position.coords.altitudeAccuracy,
-              heading: position.coords.heading,
-              speed: position.coords.speed
-            };
-            self.set('value', coords);
-          } else {
-            errors.value.push('unable to determine position');
+
+      return new Promise(function (resolve, reject) {
+        options = $.isPlainObject(options) ? options : {};
+        options = $.extend({}, defaultOptions, options);
+        geolocation.getCurrentPosition(
+          function (position) { // successCallback
+            var coords;
+            if (position.coords) {
+              coords = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                altitude: position.coords.altitude,
+                accuracy: position.coords.accuracy,
+                altitudeAccuracy: position.coords.altitudeAccuracy,
+                heading: position.coords.heading,
+                speed: position.coords.speed
+              };
+              self.set('value', coords);
+              resolve();
+            } else {
+              errors.value.push('unable to determine position');
+              self.set('errors', errors);
+              reject(new Error(errors.value[0]));
+            }
+          },
+          function (error) { // errorCallback
+            var string;
+            switch (error.code) {
+            case error.PERMISSION_DENIED:
+              string = 'user has not granted permission';
+              break;
+            case error.PERMISSION_DENIED_TIMEOUT:
+              string = 'user did not grant permission in time';
+              break;
+            case error.POSITION_UNAVAILABLE:
+              string = 'unable to determine position';
+              break;
+            default:
+              string = 'unknown error';
+            }
+            errors.value.push(string);
             self.set('errors', errors);
-          }
-        },
-        function (error) { // errorCallback
-          var string;
-          switch (error.code) {
-          case error.PERMISSION_DENIED:
-            string = 'user has not granted permission';
-            break;
-          case error.PERMISSION_DENIED_TIMEOUT:
-            string = 'user did not grant permission in time';
-            break;
-          case error.POSITION_UNAVAILABLE:
-            string = 'unable to determine position';
-            break;
-          default:
-            string = 'unknown error';
-          }
-          errors.value.push(string);
-          self.set('errors', errors);
-        },
-        options
-      );
+            reject(new Error(errors.value[0]));
+          },
+          options
+        );
+      });
     }
   });
 
