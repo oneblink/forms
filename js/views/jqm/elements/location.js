fa5dddf9a69b23b22d43ea3362cdfbb7b84e98fd
Geeta <geeta@blinkmobile.com.au>
2015-03-02 12:36:06 +1100

    Forms-72 :  Forms 3 "Location", can we have a dynamic map like Forms 2?
    - created popup for google maps
    - created popup with 3 buttons Done(to save), Cancel( to undo changes and close popup), clear (to clear map model value)
    - created function(in main) to load google map script.

--- js/views/jqm/elements/location.js
+++ js/views/jqm/elements/location.js
@@ -1,10 +1,14 @@
-define(['views/jqm/element'], function (ElementView) {
+define(['views/jqm/element',
+'text!views/jqm/templates/location.html'
+], function (ElementView, html) {
   'use strict';
   var LocationElementView = ElementView.extend({
     render: function () {
       var $button, $div;
       var i18n = window.i18n['BMP/Forms/elements'];
 
+      //load google maps
+      BMP.Forms.loadMapScript();
       this.$el.empty();
       this.renderLabel();
 
@@ -59,24 +63,129 @@ define(['views/jqm/element'], function (ElementView) {
     }
   }, {
     // static properties and methods
+    onDone: function (event) {
+      var value = $('#bmp-forms-location').find('input').val();
+
+      if (value) {
+        value = JSON.parse(value);
+        this.model.set('value', value);
+      }
+      $('#bmp-forms-location').popup('close');
+      event.preventDefault();
+      return false;
+    },
+    onClear: function (event) {
+      this.model.unset('value');
+      $('#bmp-forms-location').popup('close');
+      event.preventDefault();
+      return false;
+    },
+    onCancel: function (event) {
+      $('#bmp-forms-location').popup('close');
+      event.preventDefault();
+      return false;
+    },
     onButtonClick: function (event) {
       var i18n = window.i18n['BMP/Forms/elements'],
         view = this,
-        model = this.model;
+        model = this.model,
+        $div = $(html),
+        options = {
+          dismissible: true,
+          history: false,
+          afterclose: function (e, ui) {
+            if (e && ui) {
+              $div.remove();
+            }
+          }
+        },
+        $window = $(window),
+        $form,
+        windowX = $window.innerWidth(),
+        windowY = $window.innerHeight(),
+        value;
 
+      $(document.body).append($div);
+      $div.find('#map-canvas').height(windowY - 100);
+      $div.find('#map-canvas').width(windowX - 60);
+
+      $form = $div.children('form');
+      $form.on('submit', $.proxy(LocationElementView.onDone, this));
+
+      //disable button till location located
       view.$el.find('button').addClass('ui-disabled');
       view.$el.find('.ui-btn-text').text(i18n.LOCATING_SHORT);
 
-      model.getGeoLocation().then(function () { // onSuccess
+      value = model.get('value');
+      if (!value) {
+        model.getGeoLocation().then(function () { // onSuccess
+          value = model.get('value');
+          LocationElementView.initializeMap(value, $div);
+          //enable location button
+          view.$el.find('button').removeClass('ui-disabled');
+          view.$el.find('.ui-btn-text').text(i18n.LOCATION_BUTTON);
+        }, function () { // onError (err)
+          view.$el.find('button').removeClass('ui-disabled');
+          view.$el.find('.ui-btn-text').text(i18n.LOCATION_BUTTON);
+        });
+      } else {
+        $div.find('[data-action=clear]').removeClass('ui-disabled');
+        LocationElementView.initializeMap(value, $div);
         view.$el.find('button').removeClass('ui-disabled');
         view.$el.find('.ui-btn-text').text(i18n.LOCATION_BUTTON);
-      }, function () { // onError (err)
-        view.$el.find('button').removeClass('ui-disabled');
-        view.$el.find('.ui-btn-text').text(i18n.LOCATION_BUTTON);
-      });
+      }
 
+      //finally creating and poping up map
+      $form.trigger('create');
+      $div.popup(options);
+      $div.popup('open');
+      $form.on('click', '[data-action=clear]',
+        $.proxy(LocationElementView.onClear, this));
+      $form.on('click', '[data-action=cancel]',
+        $.proxy(LocationElementView.onCancel, this));
       event.preventDefault();
       return false;
+    },
+    initializeMap: function (value, $div) {
+      var warning = window.i18n['BMP/Forms/warning'],
+        map,
+        myLatlng,
+        mapOptions,
+        marker,
+        position;
+
+      //creating map
+      myLatlng = new google.maps.LatLng(value.latitude, value.longitude);
+      mapOptions = {
+          zoom: 8,
+          center: myLatlng,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      try {
+        map = new google.maps.Map($div.find('#map-canvas')[0], mapOptions);
+      } catch (err) {
+        $div.find('#map-canvas').text(warning.ERROR_GOOGLE_MAP);
+      }
+
+      //creating draggable marker
+      marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        draggable: true
+      });
+      //adding listener to marker dragend event
+      google.maps.event.addListener(marker, 'dragend', function(){
+        position = {
+          latitude: marker.getPosition().lat(),
+          longitude: marker.getPosition().lng(),
+          altitude: null,
+          accuracy: null,
+          altitudeAccuracy: null,
+          heading: null,
+          speed: null
+        };
+        $div.find('input').val(JSON.stringify(position));
+      });
     }
   });
 
