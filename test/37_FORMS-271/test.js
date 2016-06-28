393092fad365535798050e188a6c4c639fc84a6e
Ron Waldon <jokeyrhyme@gmail.com>
2016-06-28 15:55:52 +1000

    FORMS-271 # add error list order tests that fail

---
+++ test/37_FORMS-271/test.js
@@ -0,0 +1,70 @@
+define(['jquery', 'BlinkForms', 'BIC'], function ($, Forms) {
+  var assert = window.assert;
+
+  var types = [ 'draw', 'file', 'location', 'subForm', 'boolean', 'select',
+    'multi', 'number', 'telephone', 'password', 'email', 'url', 'text',
+    'textarea' ];
+
+  var mockForm = {
+    add: function () {},
+    getPage: function () { return mockForm; }
+  };
+
+  types.forEach(function (type) {
+    suite('37: FORMS-271: ' + type, function () {
+      var content$;
+      var model;
+      var view;
+
+      setup(function () {
+        content$ = $('[data-role="content"]');
+        model = Forms._models.Element.create({
+          name: type,
+          required: true,
+          type: type
+        }, mockForm);
+        model.initializeView();
+        view = model.attributes._view;
+      });
+
+      suite('view rendering precedes model validation', function () {
+        setup(function () {
+          view.render();
+          view.$el.appendTo(content$);
+          if (typeof view.onAttached === 'function') {
+            view.onAttached();
+          }
+          model.isValid();
+        });
+
+        test('error list is beneath field input', function () {
+          var last$ = view.$el.children().last();
+          assert.equal(last$[0].tagName.toLowerCase(), 'ul');
+        });
+      });
+
+      suite('model validation precedes view rendering', function () {
+        setup(function () {
+          model.isValid();
+          view.render();
+          view.$el.appendTo(content$);
+          if (typeof view.onAttached === 'function') {
+            view.onAttached();
+          }
+        });
+
+        test('error list is beneath field input', function () {
+          var last$ = view.$el.children().last();
+          assert.equal(last$[0].tagName.toLowerCase(), 'ul');
+        });
+      });
+
+      // teardown(function () {
+      //   model.close();
+      //   model = null;
+      //   view = null;
+      //   content$.empty();
+      // });
+    }); // END: suite('FORMS-271', ...)
+  });
+});
